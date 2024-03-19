74bb559f2b1384b20f318aa14428791abecd739c
Wim de Groot <34519486+wim-de-groot@users.noreply.github.com>
2024-03-19 09:54:51 +0100

    feat: basic platform based on omni and argocd + more
    Refs: #1
    Signed-off-by: Wim de Groot <34519486+wim-de-groot@users.noreply.github.com>

---
+++ .github/renovate-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	branchPrefix: 'dependency-update/',
+	gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+	onboarding: false,
+	platform: 'github',
+	repositories: ['wim-de-groot/4546B'],
+	packageRules: [
+	],
+};
